16446c9d1f874b6839dfcf15f0cb4c4c87f05795
mkpatel-247 <meet.codes247@gmail.com>
2024-09-11 21:31:00 +0530

    Add charts into project.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,41 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from 'src/shared/components/card/card.component';
 import { RecentTransactionsComponent } from 'src/shared/components/recent-transactions/recent-transactions.component';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle,
+  NgApexchartsModule,
+  ApexNonAxisChartSeries
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, CardComponent, RecentTransactionsComponent],
+  imports: [CommonModule, CardComponent, RecentTransactionsComponent, NgApexchartsModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  data: any = '';
-  config: any = '';
+
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: {
+    series: ApexAxisChartSeries | ApexNonAxisChartSeries;
+    chart: ApexChart;
+    xaxis: ApexXAxis;
+    title: ApexTitleSubtitle;
+  };
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "My-series",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "bar"
+      },
+      title: {
+        text: "Weekly Spending"
+      },
+      xaxis: {
+        categories: []
+      }
+    };
+  }
 
   ngOnInit(): void {
+    this.chartOptions.series = [
+      {
+        name: 'Sample Data',
+        data: [10, 20, 30, 40],
+      },
+    ];
 
-    this.data = {
-      labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-      ],
-      datasets: [{
-        label: 'My First Dataset',
-        data: [300, 50, 100],
-        backgroundColor: [
-          'rgb(255, 99, 132)',
-          'rgb(54, 162, 235)',
-          'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-      }]
-    };
-    this.config = {
-      type: 'doughnut',
-      data: this.data,
-    };
+    this.chartOptions.xaxis = {
+      categories: ["food", "rent", "travel", "other"]
+    }
   }
 }
