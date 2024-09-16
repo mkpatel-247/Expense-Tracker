14b0e6cff3020e85a830640f8d9b122d193f8319
mkpatel-247 <meet.codes247@gmail.com>
2024-09-16 23:42:37 +0530

    Installed apex-charts successfully.

---
+++ src/app/dashboard/dashboard.data.ts
@@ -0,0 +1,14 @@
+export const RECENT_TRANSACTION = [
+    { date: "", category: "", description: "", amount: "" }
+]
+
+export const TOTAL_EXPENSE = { month: "", currentMonth: "" }
+
+
+export const CATEGORY_INFO = [
+    { categoryID: '', categoryName: '', categoryExpense: '', month: '' }
+]
+
+export const MONTHLY_BUDGET = {
+    month: '', budget: '', remaining: ''
+}
